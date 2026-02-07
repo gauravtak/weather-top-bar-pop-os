bbd3aac3de09dd99e791a57bba012a0967c9cab0
gauravtak <gauravtak996@gmail.com>
2026-02-08 00:15:14 +0530

    add null check

--- weather.sh
+++ weather.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 location=$(curl -s https://ipinfo.io/json)
 city=$(echo "$location" | jq -r '.city')
-url="http://api.openweathermap.org/data/2.5/weather?APPID=<YOUR_API_KEY>&q="$city"&units=metric"
+
+if [ "$city" = "null" ]; then
+  city="Jaipur"
+fi
+
+url="http://api.openweathermap.org/data/2.5/weather?APPID=8cb0265b0c05c7588727a29517bf0806&q="$city"&units=metric"
 res=$(curl -s "$url")
 
 read temp name <<< $(echo "$res" | jq -r '"\(.main.temp) \(.name)"')
+
 
 echo "$name | $temp°C"
